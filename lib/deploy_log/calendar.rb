0b406a36732359996fe9d2a66e13f3e586c0b2ba
Ryan Priebe <rpriebe@me.com>
2019-06-12 15:43:54 -0600

    testing

--- lib/deploy_log/calendar.rb
+++ lib/deploy_log/calendar.rb
@@ -2,8 +2,33 @@
 
 module DeployLog
   class Calendar
-    def self.start_of_week(week_num)
-      puts week_num
+    def start_of_week(week_num)
+      # puts "THIS IS WEEK #{Date.today.cweek}"
+      cal = year_calendar(2019).to_a#[Date.today.cweek]
+      puts cal.inspect
+      cal[:first]
+    end
+
+    private
+
+    def range_for(year)
+      start = Date.parse("#{year}-01-01")
+      finish = Date.parse("#{year}-12-31")
+
+      (start..finish)
+    end
+
+    def year_calendar(year)
+      date_range = range_for(year)
+      output = {}
+
+      date_range.each do |day|
+        output[day.cweek] = {}
+        output[day.cweek][:first] = day
+        # output[day.cweek][:last] = day
+      end
+      # puts output.inspect
+      output
     end
   end
 end
--- lib/deploy_log/github/deploys.rb
+++ lib/deploy_log/github/deploys.rb
@@ -5,6 +5,7 @@ module DeployLog
     class Deploys
       def initialize
         @github = Helper.new(ARGV.first)
+        @calendar = DeployLog::Calendar.new
       end
 
       def merged_between(start, finish = nil)
@@ -33,10 +34,10 @@ module DeployLog
       def merged_during_week(week_num)
         return Notify.error 'Week number (--week|-w) is a required argument' if week_num.nil?
 
-        start = DeployLog::Calendar.start_of_week(24)
-        finish = start + 24 * 60 * 60 - 1
+        start = @calendar.start_of_week(24)
+        # finish = start + 24 * 60 * 60 - 1
 
-        @github.pulls_in_timeframe(start, finish)
+        # @github.pulls_in_timeframe(start, finish)
       end
 
       def pr_title(title)
